8347e9e4e8fc30e2f0a45cf24383e8d252b437ca
Lucas Aguiar Correa <lucas.aguiar1967@gmail.com>
2026-03-09 19:02:53 -0300

    Hoje estudei mais sobre as tabelas no SQL

--- Tabela.sql
+++ Tabela.sql
@@ -1,19 +1,10 @@
-
-*\ Criação Do Banco de Dados.
- primeiro código executável. podemos observar que o código configura o banco para suportar caracteres especiais e evita a diferenciação entre letras maiúsculas e minúsculas.
-
-create database cadastro;
-CHARACTER SET utf8mb4
-COLLATE utf8mb4_general_ci;
-
-
-*\ Criação da tabela ´pessoas´ dentro do banco: cadastro
-
-create table pessoas(
-nome varchar(30) NOT NULL,
-nascimento date,
-sexo enum('M', 'F'), 
-peso decimal(5,2),
-altura decimal(3,2),
-nacionalidade varchar(20) DEFAULT 'Brasil'
-)default charset = utf8;
+create table `pessoas1`(
+`id`int not null AUTO_INCREMENT,
+`nome`varchar(30) not null,
+`nascimento`date,
+`sexo`enum("M","F"),
+`peso`decimal(5,2),
+`altura`decimal(3,2),
+nacionalidade varchar(35) DEFAULT 'Brasil',
+ PRIMARY KEY(id)
+) DEFAULT charset = utf8;
